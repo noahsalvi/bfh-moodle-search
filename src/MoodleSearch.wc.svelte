f5846bfc991ee5e980a066ad320290de22d3eb26
Noah Salvi <noahsalvi@me.com>
2021-11-05 13:48:43 +0100

    remove test

--- src/MoodleSearch.wc.svelte
+++ src/MoodleSearch.wc.svelte
@@ -2,16 +2,6 @@
 
 <script>
   import MoodleSearch from "./MoodleSearch.svelte";
-
-  document.body.style = "animation: spin-me 2s linear infinite";
-  let i = 0;
-  const style = document.createElement("style");
-  style.innerText = `
-@keyframes spin-me {
-  from {transform: rotate(0deg);}
-  to {transform: rotate(360deg);}
-}`;
-  document.head.append(style);
 </script>
 
 <MoodleSearch />
